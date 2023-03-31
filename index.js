ec71865d7903471c9b06296f7c7a9d8c55cd91c8
rafy <mohammadrafy1001@gmail.com>
2023-04-01 05:53:23 +0600

    checkd23

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function sendImage(sender, url) {
 			attachment :{
 				type : "image",
 				payload : {
-					url : "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+					url :"http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_brown.png"
 				}
 			}
 			
