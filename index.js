f3708ccf78df957c9325457000758d79b676639f
raeef <70939973+raeef1001@users.noreply.github.com>
2024-01-08 01:47:36 +0600

    Update index.js
    modified it to use as a third server for winchat

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import request from "request";
 import bodyParser from "body-parser";
 import express from "express";
 import fetch from "node-fetch";
+const fs = require('fs');
 const app = express();
 
 // site runner 
@@ -11,22 +12,17 @@ let intervalID;
 
 function repeatEverySecond() {
   intervalID = setInterval(sendMessage, 120000);
- intervalID = setInterval(sendIut, 120000);
+
 }
 
 function sendMessage() {
   console.log("called")
-    fetch('https://chatgpt-messenger-dxpa.onrender.com')
+    fetch('https://chatgptmessenger-z472.onrender.com/')
     .then(response =>console.log("chat_server_is_running") )
 
    
 }
-function sendIut() {
-  console.log("called iutcs")
-   fetch('https://iut-cs-backend.onrender.com/api/blogs?populate=*')
-    .then(response =>console.log(response) )
-   
-}
+
 
 repeatEverySecond()
 
@@ -39,9 +35,41 @@ app.use(bodyParser.json());
 // ROUTES
 
 app.get("/", function (req, res) {
-  res.send("fucking chatbot");
+  res.send("fucking chatbot 111");
 });
 
+// Initialize storedData with data from the file if available
+let storedData = [];
+
+const storedDataFilePath = 'storedData.txt';
+
+// Read data from the file and initialize storedData
+if (fs.existsSync(storedDataFilePath)) {
+    const fileData = fs.readFileSync(storedDataFilePath, 'utf-8');
+    try {
+        storedData = JSON.parse(fileData);
+    } catch (error) {
+        console.error('Error parsing stored data file:', error);
+    }
+}
+
+// Endpoint to receive data and store it in a text file
+app.post('/storeData', (req, res) => {
+    const newData = req.body;
+
+    // Assuming newData has the format { name, ip, port }
+    storedData.push(newData);
+
+    // Update the text file with the new data
+    fs.writeFileSync(storedDataFilePath, JSON.stringify(storedData));
+
+    res.status(200).send('Data stored successfully');
+});
+
+// Endpoint to get all stored data
+app.get('/getAllData', (req, res) => {
+    res.json(storedData);
+});
 
 
 // all time listner 
